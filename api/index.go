a7d72a33e761c02430114890b2563127506b8941
apinanyogaratnam <apinan@ontopical.com>
2023-07-16 19:22:27 -0400

    fix: index.go module

--- api/index.go
+++ api/index.go
@@ -1,13 +1,13 @@
 package handler
 
 import (
-	"net/http"
 	"encoding/json"
+	"net/http"
 )
 
 func Root(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode("Hello World")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
 }
 
 func Handler(w *http.ResponseWriter, r *http.Request) {
